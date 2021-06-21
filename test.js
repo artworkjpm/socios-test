680f9d712615b81a195e0ab8fec3cdc87252c209
JohnMoran <johnnymoran44@hotmail.com>
2021-06-21 09:26:24 +0200

    test explaination

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 function monthFormat(numberToCheck) {
-	console.log(numberToCheck);
-	if (numberToCheck <= 0) {
-		console.log("passed");
+	if (!numberToCheck <= 0) {
+		console.log("shows true because ! + -1 converts to true");
 	} else {
 		console.log("failed");
-		return "";
 	}
 }
 
 monthFormat(-1);
+
+//you would think this code would go to 'failed', but it doesn't. If you read the code it says if -1 is NOT less than or equal to zero, but of course it is less than 0. So what happens? Because we have a negative number: -1 used with !, it converts the statement to be the opposite (two negatives make positive), so instead we are saying IS -1 less than 0, and of course it is. To get over this you can use !! or remove the !.
